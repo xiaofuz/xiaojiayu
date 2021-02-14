87b1b675634f2752841b2c3b837ab2413217e569
xiaofuz <981276532@qq.com>
2021-02-14 10:11:56 +0800

    Python

---
+++ Python/浙大版（Python程序设计）/题目练习.py
@@ -0,0 +1,17 @@
+# S = 11
+# m = input()
+# if 20<m<=100:
+#     while S <= m:
+#         S++
+#     sum = S
+# else
+#     print("请输入大于20小于等于100的数")
+# print("sum = ")
+
+
+
+m = input()
+while (range(11,m)):
+    s ++
+    sum += s
+    
